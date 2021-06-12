6469a761bfe58a16ba16848d9a159412f5185ab5
Igor Tudisco <igortudisco@hotmail.com>
2021-06-11 22:38:16 -0300

    Exercícios Collections

---
+++ BlocoI/src/Collections/EstoqueMain.java
@@ -0,0 +1,25 @@
+package Collections;
+
+public class EstoqueMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 3-	Crie uma um programa para trabalhar com estoque de uma loja,
+		 * o programa deverá trabalhar com Collection do tipo List do Java
+		 * para manipular os dados desse estoque, o programa deverá atender
+		 * as seguintes funcionalidades:
+		
+				Armazenar dados da List
+				Remover dados da list;
+				Atualizar dados da list.
+				Apresentar todos os dados da list.
+		
+		 */
+		
+		
+
+	}
+
+}
